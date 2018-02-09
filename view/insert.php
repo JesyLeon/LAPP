1b83d3e2694073fa9cc9cf18a8e6946ebcf23059
JesyLeon <jessica.leon@coficab.com>
2018-02-09 09:57:01 -0600

    test

--- view/insert.php
+++ view/insert.php
@@ -1,34 +1,5 @@
-<?php
-    class Clasedb{
-    private $conexion;
-    public function MySQL(){
-      if(!isset($this->conexion)){
-      $this->conexion = (mysql_connect("localhost","root","")) or die(mysql_error());
-      mysql_select_db("lapp",$this->conexion) or die(mysql_error());
-      }
-      }
-    public function consulta($consulta){
-      $resultado = mysql_query($consulta,$this->conexion);
-      if(!$resultado){
-      echo 'MySQL Error: ' . mysql_error();
-      exit;
-      }
-      return $resultado;  
-      }
-    public function obtenerfilas($consulta){  
-      return mysql_fetch_array($consulta);
-      }
-    public function totalfilas($consulta){  
-      return mysql_num_rows($consulta);
-      }
-     
-    } ?>
 <? error_reporting(0);
     include("../model/connection.php");
-    //Instaciamos la clase de base de datos
-    $db = new Clasedb();
-    //Llamamos a la funcion para conectar a la base de datos
-    $db->MySQL();
      
     //leemos los datos enviados y los guardamos en matrices
     $location=$_POST['location'];
@@ -41,9 +12,9 @@
     	 VALUES( '".$location[$i]."' , '".$capacity[$i]."')";
      
     //Grabamos los datos en la tabla personal
-    $consulta = $db->consulta($sql);	
+    $resultado = $conexion->query($sql);	
     }
      
     //volvemos a la pagina inicial
-    header('Location: ../view/crud.php');
+    header('Location: crud.php');
     ?>
